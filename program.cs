079902eafde58bce3467c22bf6ab8b71cae79ffa
Nasko Stanoev <nasko.stanoevv@gmail.com>
2022-05-26 22:10:01 +0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,17 +12,19 @@ namespace _01._Basic_Stack_Operations
 
     internal class Program
     {
+        // Settings
         static int LinesRows = 1;
         static int TetrisRows = 20;
         static int TetrisCols = 10;
         static int InfoCols = 7;
         static int ConsoleRows = 1 + LinesRows + 1 + TetrisRows + 1;
         static int ConsoleCols = 1 + TetrisCols + 1 + InfoCols + 1;
-
+        static int[] ScorePerLines = new int[] { 0, 20, 50, 150, 600 };
         //State
+        static int HightScore = 0;
         static int score = 0;
         static int Lines = 0;
-        static int Level = Lines / 10;
+        static int Level = (Lines / 10) + 1;
         static int FrameCounter = 1;
         static int FrameToMoveFigure = 16;
         static bool[,] TetrisField = new bool[TetrisRows + 3, TetrisCols + 1];
@@ -68,27 +72,39 @@ namespace _01._Basic_Stack_Operations
 
         };
         static Random random = new Random();
-        // static int NextFigureIndex = random.Next(0,Figures.Count);
+        static int NextFigureIndex = random.Next(0, Figures.Count);
         static bool[,] CurrentFigure = Figures[0];
-        static bool[,] NextFigure = Figures[0];
+        static bool[,] NextFigure = Figures[NextFigureIndex];
         static int currentFigureRow = 3;
         static int currentFigureCol = 4;
-        static int lastRow = currentFigureRow + CurrentFigure.GetLength(0) ;
-        static int lastCol = currentFigureCol + CurrentFigure.GetLength(1) ;
+        static int lastRow = currentFigureRow + CurrentFigure.GetLength(0);
+        static int lastCol = currentFigureCol + CurrentFigure.GetLength(1);
 
 
         static void Main(string[] args)
         {
+            if (File.Exists("score.txt"))
+            {
+                var allScores = File.ReadAllLines("score.txt");
+                foreach (var score in allScores)
+                {
+                    var match = Regex.Match(score, @"= (?<score>[0-9]+)");
+                    if (match.Success)
+                    {
+                        HightScore = Math.Max(HightScore, int.Parse(match.Groups[1].Value));
+                    }
+                }
+            }
             {
                 Console.Title = "Tetris v1.0";
                 Console.WindowHeight = ConsoleRows;
                 Console.WindowWidth = ConsoleCols;
-                Console.BufferHeight = ConsoleRows + 1;
+                Console.BufferHeight = ConsoleRows;
                 Console.BufferWidth = ConsoleCols;
                 Console.CursorVisible = false;
-                NextFigure = Figures[0];
+                NextFigure = Figures[random.Next(0, Figures.Count)];
                 CurrentFigure = NextFigure;
-
+                
             }
             while (true)
             {
@@ -107,12 +123,12 @@ namespace _01._Basic_Stack_Operations
                         key.Key == ConsoleKey.UpArrow ||
                         key.Key == ConsoleKey.W)
                     {
-                        //TODO: Rotate CurrentFigure
+                        RotateCurrentFigure();
                     }
                     if (key.Key == ConsoleKey.LeftArrow ||
                         key.Key == ConsoleKey.A)
                     {
-                        //TODO: Move CurrentFigure left
+                        
                         if (IsInside(currentFigureRow, currentFigureCol - 1,
                             lastRow,
                             lastCol - 1))
@@ -123,7 +139,7 @@ namespace _01._Basic_Stack_Operations
                     if (key.Key == ConsoleKey.RightArrow ||
                         key.Key == ConsoleKey.D)
                     {
-                        //TODO: Move CurrentFigure Right
+                        
                         if (IsInside(currentFigureRow, currentFigureCol + 1,
                             lastRow,
                             lastCol + 1))
@@ -134,12 +150,12 @@ namespace _01._Basic_Stack_Operations
                     if (key.Key == ConsoleKey.DownArrow ||
                         key.Key == ConsoleKey.S)
                     {
-                        
-                            FrameCounter = 1;
-                            score++;
-                            currentFigureRow++;
-                            DrawInfo(lastRow, lastCol);
-                        
+
+                        FrameCounter = 1;
+                        score++;
+                        currentFigureRow++;
+                        DrawInfo(lastRow, lastCol);
+
 
                     }
 
@@ -147,30 +163,58 @@ namespace _01._Basic_Stack_Operations
 
 
                 //Update game state
-                if (FrameCounter % FrameToMoveFigure - Level == 0)
+                if (FrameCounter % (FrameToMoveFigure - Level) == 0)
                 {
 
-                    
-                        FrameCounter = 1;
-                        
-                        currentFigureRow++;
-                        DrawInfo(lastRow, lastCol);
-                    
+
+                    FrameCounter = 1;
+
+                    currentFigureRow++;
+                    DrawInfo(lastRow, lastCol);
+
 
                 }
-                if (Collisium())
+                if (Collisium(CurrentFigure))
                 {
                     DrawInfo(lastRow, lastCol);
                     AddCurrentFigureToTetrisField();
+                    for (int i = 0; i < CurrentFigure.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < CurrentFigure.GetLength(1); j++)
+                        {
+                            if (CurrentFigure[i, j])
+                            {
+                                score++;
+                            }
+                        }
+                    }
+                    int currentLines = CheckForFullLines();
+                    score += ScorePerLines[currentLines] * Level;
+                    Lines += currentLines;
+                    Level = (Lines / 10) + 1;
                     CurrentFigure = NextFigure;
-                    NextFigure = Figures[0];
+                    NextFigure = Figures[NextFigureIndex = random.Next(0, Figures.Count)];
                     currentFigureRow = 3;
                     currentFigureCol = 4;
-                    //    //TODO: ChekForFullLines();
-                    //    //If (lines removed)
-                    //    //{
-                    //    //Score++; 
-                    //    //}
+                    if (Collisium(CurrentFigure))
+                    {
+                        File.AppendAllLines("score.txt", new List<string>
+                        {
+                        $"[{DateTime.Now.ToString()}] {Environment.UserName}= {score}"
+                        });
+                        // Console.Clear();
+                        // TODO: NewHightScore
+                        string scoreAsStirng = score.ToString();
+                        scoreAsStirng += new string(' ', 6 - scoreAsStirng.Length);
+                        Write("╔══════════╗", 4, 8);
+                        Write("║ GAME     ║", 4, 9);
+                        Write("║  OVER!   ║", 4, 10);
+                        Write("║   SCORE: ║", 4, 11);
+                        Write($"║  {scoreAsStirng}  ║", 4, 12);
+                        Write("╚══════════╝", 4, 13);
+                        Thread.Sleep(100000000);
+                        return;
+                    }
                 }
 
                 //Redeaw UI
@@ -178,30 +222,86 @@ namespace _01._Basic_Stack_Operations
                 DrawInfo(lastRow, lastCol);
                 DrawTetrisField();
                 DrawCurrentFigure(CurrentFigure);
-                // DrawNextFigure(NextFigure);
+                DrawNextFigure(NextFigure);
 
                 Thread.Sleep(40);
             }
 
         }
-        static bool Collisium()
+
+        private static void RotateCurrentFigure()
         {
-            if (currentFigureRow-1+CurrentFigure.GetLength(0) == TetrisField.GetLength(0))
+            var newFigure = new bool[CurrentFigure.GetLength(1),CurrentFigure.GetLength(0)];
+            for (int i = 0; i < CurrentFigure.GetLength(0); i++)
+            {
+                for (int j = 0; j < CurrentFigure.GetLength(1); j++)
+                {
+                    newFigure[j, CurrentFigure.GetLength(0) - i - 1] = CurrentFigure[i, j];
+                }
+            }
+            if (!Collisium(newFigure))
+            {
+            CurrentFigure = newFigure;
+
+            }
+        }
+
+        private static int CheckForFullLines()
+        {
+            int lines = 0;
+
+            for (int i = TetrisField.GetLength(0) - 1; i >= 4; i--)
+            {
+                bool RowIsFull = true;
+                for (int j = 1; j < TetrisField.GetLength(1); j++)
+                {
+                    if (!TetrisField[i, j])
+                    {
+                        RowIsFull = false;
+                        break;
+                    }
+                }
+                if (RowIsFull)
+                {
+                    for (int k = i; k >= 4; k--)
+                    {
+                        for (int c = 0; c < TetrisField.GetLength(1); c++)
+                        {
+                            TetrisField[k, c] = TetrisField[k - 1, c];
+                        }
+                    }
+                    lines++;
+                }
+            }
+            if (lines>1)
+            {
+                return 2;
+            }
+            return lines;
+        }
+
+        static bool Collisium(bool[,] figure)
+        {
+            if (currentFigureCol>TetrisField.GetLength(1)-figure.GetLength(1))
             {
                 return true;
             }
-                for (int i = 0; i < CurrentFigure.GetLength(0); i++)
+            if (currentFigureRow - 1 + figure.GetLength(0) == TetrisField.GetLength(0))
+            {
+                return true;
+            }
+            for (int i = 0; i < figure.GetLength(0); i++)
+            {
+                for (int j = 0; j < figure.GetLength(1); j++)
                 {
-                    for (int j = 0; j < CurrentFigure.GetLength(1); j++)
+                    if (figure[i, j]
+                        && TetrisField[currentFigureRow + i, currentFigureCol + j])
                     {
-                        if (CurrentFigure[i, j]
-                            && TetrisField[currentFigureRow + i , currentFigureCol + j])
-                        {
-                            return true;
-                        }
+                        return true;
                     }
                 }
-            
+            }
+
             return false;
         }
 
@@ -213,7 +313,7 @@ namespace _01._Basic_Stack_Operations
                 {
                     if (CurrentFigure[i, j])
                     {
-                        TetrisField[i + currentFigureRow-1, j + currentFigureCol] = true;
+                        TetrisField[i + currentFigureRow - 1, j + currentFigureCol] = true;
                     }
                 }
             }
@@ -269,7 +369,7 @@ namespace _01._Basic_Stack_Operations
                 {
                     if (TetrisField[i, j])
                     {
-                         Write("▓", j , i );
+                        Write("▓", j, i);
                     }
                 }
             }
@@ -280,9 +380,6 @@ namespace _01._Basic_Stack_Operations
             DrawLevel(Level);
             Write(Lines.ToString(), 7, 1);
             Write(score.ToString(), 12, 3);
-            Write(currentFigureRow.ToString() + ',' + currentFigureCol.ToString(), 13, 7);
-            Write(lastRow.ToString() + ',' + lastCol.ToString(), 13, 9);
-
         }
 
         static void DrawBorder()
@@ -432,7 +529,7 @@ namespace _01._Basic_Stack_Operations
                     Console.SetCursorPosition(16, 19);
                     Console.WriteLine("  ║");
                     Console.SetCursorPosition(16, 20);
-                    Console.WriteLine("  ");
+                    Console.WriteLine("  ║");
                     Console.SetCursorPosition(16, 21);
                     Console.WriteLine("  ║");
                     Console.SetCursorPosition(16, 22);
@@ -833,5 +930,8 @@ namespace _01._Basic_Stack_Operations
             Console.Write(text);
             Console.ResetColor();
         }
+        
     }
 }
+
+
